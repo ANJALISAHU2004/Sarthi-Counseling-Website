bb6e193448bfb46cea9b321aeb212cd3b1be4fee
ANJALISAHU2004 <145754956+ANJALISAHU2004@users.noreply.github.com>
2024-10-03 19:49:05 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -16,14 +16,7 @@ function App() {
       {/* <Login></Login> */}
       {/* <Footer></Footer> */}
       <Cards></Cards>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+     
     </>
   );
 }
